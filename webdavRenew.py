cc28c8671060431bdddea0c5d56fbb0b8b6775c9
Intro <1243710035@qq.com>
2024-01-18 03:18:52 +0800

    deleted:    webdavRenew.py

--- webdavRenew.py
+++
@@ -1,4 +0,0 @@
-comicID = [int(str(line.strip()).split("-")[0]) for line in open('dataList.log', 'r', encoding='utf-8')]
-with open('dataList.log', 'w') as f:
-    for comic in comicID:
-        f.write(str(comic) + ' ')
