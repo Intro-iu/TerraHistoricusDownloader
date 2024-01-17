379d3b42189cdadac9e7dceb9ec05c4213b4fd44
Intro <1243710035@qq.com>
2024-01-18 02:32:00 +0800

    modified:   getTerriaComic.py

--- getTerriaComic.py
+++ getTerriaComic.py
@@ -41,7 +41,7 @@ class Comic:
             self.totalChapter.append([])
             if option:
                 data = []
-                os.system('rclone lsf CloudDrive:' + self.comic_path + '/' + self.CID[n] + '_' + self.title[n] + ' > dataList.log && echo "Loaded"' )
+                os.system('rclone lsf CloudDrive:' + self.comic_path + '/' + self.CID[n] + '_' + self.title[n] + ' > dataList.log && cat dataList.log' )
                 with open('dataList.log', 'r', encoding='UTF-8') as f:
                     for line in f:
                         if line.strip('\n')[-1] == '/':
